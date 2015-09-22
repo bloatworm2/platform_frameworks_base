3b116dbd86c26476116451e7c7bd56eb42949a0e
Michael Lentine <mlentine@google.com>
2015-09-22 13:02:22 -0700

    Dismiss color fade when it is no longer used.
    Previously the color fade layers would stay active until the screen was turned
    back on. After this change, the layers are dismissed as soon as color fade
    completes.
    
    Bug: 24276564
    
    Change-Id: I1f81b397d82d518c2bc921b22f7fe9fd066fc059

--- services/core/java/com/android/server/display/DisplayPowerController.java
+++ services/core/java/com/android/server/display/DisplayPowerController.java
@@ -849,6 +849,7 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
         if (mPendingScreenOff && target != Display.STATE_OFF) {
             setScreenState(Display.STATE_OFF);
             mPendingScreenOff = false;
+            mPowerState.dismissColorFade();
         }
 
         if (target == Display.STATE_ON) {
@@ -922,6 +923,7 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
                 // A black surface is already hiding the contents of the screen.
                 setScreenState(Display.STATE_OFF);
                 mPendingScreenOff = false;
+                mPowerState.dismissColorFade();
             } else if (performScreenOffTransition
                     && mPowerState.prepareColorFade(mContext,
                             mColorFadeFadesConfig ?
