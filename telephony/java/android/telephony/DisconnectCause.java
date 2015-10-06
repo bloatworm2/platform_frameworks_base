449af8efa0acd00bb18b11819fc8d4e30d9e74b6
Andalam Parthasarathi <andala@codeaurora.org>
2015-10-06 18:26:24 +0530

    Add mapping string for call reject cause code 26.
    Mapping string for the specified disconnect cause.
    Show a toast string "non-selected user clearing"
    for cause code 26
    
    Change-Id: I6592411b147f1109b37b1a60a868468a8840a47a
    CRs-Fixed: 801884
    
    Conflicts:
    	telephony/java/android/telephony/DisconnectCause.java

--- telephony/java/android/telephony/DisconnectCause.java
+++ telephony/java/android/telephony/DisconnectCause.java
@@ -241,6 +241,8 @@ public class DisconnectCause {
     /** EMERGENCY call failed with permanent fail cause */
     public static final int EMERGENCY_PERM_FAILURE         = 97;
 
+    public static final int NON_SELECTED_USER_CLEARING = 98;
+
     //*********************************************************************************************
     // When adding a disconnect type:
     // 1) Please assign the new type the next id value below.
@@ -249,7 +251,7 @@ public class DisconnectCause {
     // 4) Update toString() with the newly added disconnect type.
     // 5) Update android.telecom.DisconnectCauseUtil with any mappings to a telecom.DisconnectCause.
     //
-    // NextId: 98
+    // NextId: 99
     //*********************************************************************************************
 
     /** Smallest valid value for call disconnect codes. */
@@ -364,6 +366,8 @@ public class DisconnectCause {
             return "IMS_MERGED_SUCCESSFULLY";
         case CDMA_ALREADY_ACTIVATED:
             return "CDMA_ALREADY_ACTIVATED";
+        case NON_SELECTED_USER_CLEARING:
+            return "NON_SELECTED_USER_CLEARING";
         case HO_NOT_FEASIBLE:
             return "HO_NOT_FEASIBLE";
         case NO_CIRCUIT_AVAIL:
