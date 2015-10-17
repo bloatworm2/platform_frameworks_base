69fb8addef606a782714717a691d981ad39e697e
Alex Naidis <alex.naidis@linux.com>
2015-10-17 22:28:03 +0200

    SystemUI: shut up!
    Change-Id: Ibb1e104b756475ceff7c98ad29eea23b3c593614
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
@@ -62,7 +62,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
         implements NetworkController, DemoMode {
     // debug
     static final String TAG = "NetworkController";
-    static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
+    static final boolean DEBUG = false;
     // additional diagnostics, but not logspew
     static final boolean CHATTY =  Log.isLoggable(TAG + "Chat", Log.DEBUG);
 
