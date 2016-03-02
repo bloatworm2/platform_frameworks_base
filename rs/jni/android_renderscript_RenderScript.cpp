5a81db89c4c04599cf2beeb0f5d9dc31a8b30251
Pirama Arumuga Nainar <pirama@google.com>
2016-03-01 20:37:19 -0800

    [RenderScript] Enable untyped variants of Allocation-copy functions for FLOAT_16
    http://b/27251511
    
    Handle FLOAT_16 case to PER_ARRAY_TYPE macro to enable untyped variants
    of Allocation-copy functions.
    
    Untyped variants of Allocation-copy functions call
    validateObjectIsPrimitiveArray(), with checkType parameter set to true,
    to validate and obtain the Element type of the Allocation.  When
    checkType is true, validateObjectIsPrimitiveArray() returns the actual
    Element type of the Allocation (instead of the canonical SIGNED type).
    Because of this, FLOAT_16 element type can reach the JNI layer,
    necessitating this change to PER_ARRAY_TYPE macro.
    
    Change-Id: I3651e5ae0ab0cd6bb4ad3157841e2910ac8699dc
    (cherry picked from commit 85e8c51dbf8dc774ecfd09981ac6a8bf1b75f464)

--- rs/jni/android_renderscript_RenderScript.cpp
+++ rs/jni/android_renderscript_RenderScript.cpp
@@ -139,6 +139,7 @@ void UNUSED(T... t) {}
         return;                                                                         \
     case RS_TYPE_SIGNED_16:                                                             \
     case RS_TYPE_UNSIGNED_16:                                                           \
+    case RS_TYPE_FLOAT_16:                                                              \
         len = _env->GetArrayLength((jshortArray)data);                                  \
         ptr = _env->GetShortArrayElements((jshortArray)data, flag);                     \
         typeBytes = 2;                                                                  \
