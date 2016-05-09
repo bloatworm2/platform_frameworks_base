4abaffb98481fc5321c0f4bd384e8ccebd298fbe
Pat Erley <perley@cyngn.com>
2016-05-09 14:35:00 -0700

    ShutdownThread: Decrypt before stopping storage manager
    uncrypt needs access to the file to uncrypt it.  If we stop the
    storage manager service, uncrypting fails, leading to failed
    updates.
    
    Change-Id: Ifc75c990ebf8f0a2f11b340cd39cd469b46c6f68

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -602,6 +602,13 @@ public final class ShutdownThread extends Thread {
             }
         };
 
+        if (mRebootUpdate) {
+            sInstance.setRebootProgress(MOUNT_SERVICE_STOP_PERCENT, null);
+
+            // If it's to reboot to install update, invoke uncrypt via init service.
+            uncrypt();
+        }
+
         Log.i(TAG, "Shutting down MountService");
 
         // Set initial variables and time out time.
@@ -636,12 +643,6 @@ public final class ShutdownThread extends Thread {
                 } catch (InterruptedException e) {
                 }
             }
-        }
-        if (mRebootUpdate) {
-            sInstance.setRebootProgress(MOUNT_SERVICE_STOP_PERCENT, null);
-
-            // If it's to reboot to install update, invoke uncrypt via init service.
-            uncrypt();
         }
 
         rebootOrShutdown(mContext, mReboot, mRebootReason);
