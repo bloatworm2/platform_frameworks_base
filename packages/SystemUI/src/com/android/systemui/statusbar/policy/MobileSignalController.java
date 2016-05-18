c3efa85667a4690671960b06c2317b806a457618
Ethan Chen <intervigil@gmail.com>
2016-05-18 16:28:59 -0700

    SystemUI: Check current data subscription when updating signal cluster
    * Checking connected and data state is insufficient when updating the
      signal cluster, for multi-SIM use we need to check whether the current
      signal controller has the data subscription as well, otherwise it's
      possible for both clusters to display the data tech simultaneously.
    
    REF: KIPPER-702
    Change-Id: I1dd2994271df2c42deb8e9aeb7c01798a6cfd2ca

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -395,7 +395,8 @@ public class MobileSignalController extends SignalController<
             mCurrentState.iconGroup = mDefaultIcons;
         }
         mCurrentState.dataConnected = mCurrentState.connected
-                && mDataState == TelephonyManager.DATA_CONNECTED;
+                && mDataState == TelephonyManager.DATA_CONNECTED
+                && mCurrentState.dataSim;
 
         if (isCarrierNetworkChangeActive()) {
             mCurrentState.iconGroup = TelephonyIcons.CARRIER_NETWORK_CHANGE;
